5baa5b3251cd88aac5e774a7bf79db85ca9b3cde
Michael Stewart <tgm@vt.edu>
2015-08-17 14:50:13 -0400

    yo angular'd

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wetWouteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wetWouteApp
+ */
+angular.module('wetWouteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wetWouteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wetWouteApp
+ */
+angular.module('wetWouteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
